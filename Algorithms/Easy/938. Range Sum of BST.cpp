8b0ca7a4558127d777a2270363d9b46de65f7465
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-10-29 14:39:20 +0530

    Create 938. Range Sum of BST.cpp

---
+++ Algorithms/Easy/938. Range Sum of BST.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if(root == NULL) return 0;
+        else if(root->val>=L && root->val<=R) return ((root->val+rangeSumBST(root->left,L,R))+rangeSumBST(root->right,L,R));
+        else return (rangeSumBST(root->left,L,R)+rangeSumBST(root->right,L,R));
+    }
+};
