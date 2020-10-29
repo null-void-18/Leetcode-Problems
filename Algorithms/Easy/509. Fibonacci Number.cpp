03a62e8b29b325fbd3f7eb3ee9cafa14b9307828
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-10-29 14:38:07 +0530

    Create 509. Fibonacci Number.cpp

---
+++ Algorithms/Easy/509. Fibonacci Number.cpp
@@ -0,0 +1,29 @@
+Iterative Solution:
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int a=0,b=1,c;
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+};
+
+
+
+Recursive Solution:
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        else if(n==1) return 1;
+        else return (fib(n-1)+fib(n-2));
+    }
+};
