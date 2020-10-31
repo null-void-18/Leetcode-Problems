38b430107685e6fe191b1d95fbbf8301f1428e3a
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-10-31 18:59:28 +0530

    Update 144. Binary Tree Preorder Traversal.cpp

--- Algorithms/Medium/144. Binary Tree Preorder Traversal.cpp
+++ Algorithms/Medium/144. Binary Tree Preorder Traversal.cpp
@@ -1,3 +1,26 @@
+Iterative Solution:
+
+class Solution {
+public:   
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> a;
+        while(root!=NULL || !a.empty()){
+            while(root!=NULL){
+                res.push_back(root->val);
+                a.push(root);
+                root=root->left;
+            }
+            root=a.top();
+            a.pop();
+            root=root->right;
+        }
+        return res;
+    }
+};
+
+
+
 Recursive Solution:
 
 class Solution {
