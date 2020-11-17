538c7f0e260c39ba699d8fcfc5a54d7ebf8570b9
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-11-17 09:55:25 +0530

    Update 1652. Defuse the Bomb.cpp

--- Algorithms/Easy/1652. Defuse the Bomb.cpp
+++ Algorithms/Easy/1652. Defuse the Bomb.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-	vector<int> decrypt(vector<int>& code, int k) {
-		int n = code.size(), i, j;
-		vector<int> a(n, 0);
-		for (i = 0; i < n; i++) {
-			if (k > 0) {
-				for (j = 1; j <= k; j++) 
-          a[i] += code[(i + j) % n];
-			}
-			if (k < 0) {
-				for (j = 1; j <= -k; j++) 
-          a[i] += code[(i - j + n) % n];
-			}
-		}
-		return a;
-	}
+    vector<int> decrypt(vector<int>& code, int k) {
+        int sum,n=code.size();
+        vector<int> a(n,0);
+        
+        for(int i=0;i<n;i++){
+            sum=0;
+            if(k>0){
+                for(int j=1;j<=k;j++){
+                    sum+=code[(i+j)%n];
+                }
+            }
+            if(k<0){
+                for(int j=1;j<=-k;j++){
+                    sum+=code[(i-j+n)%n];
+                }
+            }
+            a[i]=sum;
+        }
+        return a;
+    }
 };
