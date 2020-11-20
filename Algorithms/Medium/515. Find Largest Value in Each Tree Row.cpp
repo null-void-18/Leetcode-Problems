331e957e28ff7da6e30c4c0facc98b6a77dbe564
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-11-20 19:50:34 +0530

    Create 515. Find Largest Value in Each Tree Row.cpp

---
+++ Algorithms/Medium/515. Find Largest Value in Each Tree Row.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int height(TreeNode* p){
+        if(p == NULL) return 0;
+        return 1+max(height(p->left),height(p->right));
+    }
+    
+    void dfs(TreeNode* p,vector<int> &temp,int d,int n){
+        if(p == NULL) return;
+        if(n==d) temp.push_back(p->val);
+        dfs(p->left,temp,d,n+1);
+        dfs(p->right,temp,d,n+1);
+        return;
+    }
+    
+    vector<int> largestValues(TreeNode* root) {
+        int d=height(root);
+        if(root == NULL) return {};
+        vector<int> ans,temp;
+        ans.push_back(root->val);
+        for(int i=2;i<=d;i++){
+            dfs(root,temp,i,1);
+            ans.push_back(*max_element(temp.begin(),temp.end()));
+            temp.clear();
+        }
+        return ans;
+    }
+};
