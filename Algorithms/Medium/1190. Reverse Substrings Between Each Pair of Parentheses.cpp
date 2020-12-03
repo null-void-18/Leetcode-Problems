0d2b3bda1b12c2f20bcc1ab2a6bacec9d095f2b2
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-12-03 12:05:03 +0530

    Create 1190. Reverse Substrings Between Each Pair of Parentheses.cpp

---
+++ Algorithms/Medium/1190. Reverse Substrings Between Each Pair of Parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<pair<char,int>> st;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                st.push({'(',i});
+            }
+            else if(s[i]==')'){
+                cout<<*(s.begin()+i-1);
+                reverse(s.begin()+st.top().second+1,s.begin()+i);
+                s.erase(i,1);
+                s.erase(st.top().second,1);
+                i=i-2;
+                st.pop();
+            }
+        }
+        return s;
+    }
+};
