8bda4fadf9867d120a9c32c2838eb2e651dadf4e
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2020-12-04 10:53:50 +0530

    Create 503. Next Greater Element II.cpp

---
+++ Algorithms/Medium/503. Next Greater Element II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  vector<int> nextGreaterElements(vector<int>& nums) {
+    stack<int> st;
+    vector<int> ans(nums.size(), -1);
+    for (int i = 0; i < nums.size(); ++i) {
+      while (!st.empty() && nums[st.top()] < nums[i]) {
+        ans[st.top()] = nums[i];
+        st.pop();
+      }
+      st.push(i);
+    }
+    for (int i = 0; i < nums.size(); ++i) {
+      while (!st.empty() && nums[st.top()] < nums[i]) {
+        ans[st.top()] = nums[i];
+        st.pop();
+      }
+      st.push(i);
+    }
+    return ans;
+  }
+};
