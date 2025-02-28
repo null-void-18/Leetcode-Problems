b2c13d74bc0d35534b39a191af4eec5133f34b98
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-02-28 14:19:35 +0530

    Update Course Schedule.java

--- Java-practice/Course Schedule.java
+++ Java-practice/Course Schedule.java
@@ -1,5 +1,5 @@
 
-//Topo sort, Kahn's algo
+//Topo sort, Kahn's algo - Course Schedule II 
 
 class Solution {
     public static int[] findOrder(int n, int m, ArrayList<ArrayList<Integer>> prerequisites) {
@@ -42,5 +42,54 @@ class Solution {
         }
 
         return order.stream().mapToInt(i -> i).toArray();
+    }
+}
+
+
+
+
+
+//Course Schedule I - Leetcode - DFS Cycle detection
+
+class Solution {
+
+    private boolean dfs(List<List<Integer>> adj,boolean vis[],boolean rec[],int start) {
+        vis[start] = true;
+        rec[start] = true;
+
+        for(int i : adj.get(start)) {
+            if(!vis[i]) {
+                if(dfs(adj,vis,rec,i)) {
+                    return true; 
+                }
+            }else if(rec[i] == true) {
+                return true;
+            }
+        }
+        rec[start] = false;
+        return false;
+    }
+
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        boolean vis[] = new boolean[numCourses];
+        boolean rec[] = new boolean[numCourses];
+        for(int i = 0; i < numCourses;i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] it : prerequisites) {
+            adj.get(it[1]).add(it[0]);
+        }
+
+        for(int i = 0; i < numCourses;i++) {
+            if(!vis[i]) {
+                if(dfs(adj,vis,rec,i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
