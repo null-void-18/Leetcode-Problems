29e50f5bf326a1486ffee610cff98988a7d3314f
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-03-01 13:46:23 +0530

    Update Shortest path in Directed Acyclic Graph.java

--- Java-practice/Shortest path in Directed Acyclic Graph.java
+++ Java-practice/Shortest path in Directed Acyclic Graph.java
@@ -1,3 +1,72 @@
+//DFS Approach
+
+
+class Solution {
+    static class Pair {
+        int vertex, weight;
+        Pair(int v, int w) {
+            this.vertex = v;
+            this.weight = w;
+        }
+    }
+
+    public int[] shortestPath(int N,int E, int[][] edges) {
+        int source = 0;
+        List<List<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(new Pair(edge[1], edge[2]));
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                topoSort(i, visited, stack, adj);
+            }
+        }
+
+        int[] dist = new int[N];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[source] = 0;
+
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            if (dist[node] != Integer.MAX_VALUE) {
+                for (Pair neighbor : adj.get(node)) {
+                    int newDist = dist[node] + neighbor.weight;
+                    if (newDist < dist[neighbor.vertex]) {
+                        dist[neighbor.vertex] = newDist;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                dist[i] = -1;
+            }
+        }
+
+        return dist;
+    }
+
+    private void topoSort(int node, boolean[] visited, Stack<Integer> stack, List<List<Pair>> adj) {
+        visited[node] = true;
+        for (Pair neighbor : adj.get(node)) {
+            if (!visited[neighbor.vertex]) {
+                topoSort(neighbor.vertex, visited, stack, adj);
+            }
+        }
+        stack.push(node);
+    }
+}
+
+
+
 
 
 //BFS Approach
