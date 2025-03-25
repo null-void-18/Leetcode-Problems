4774c81cf123004a5db47d6778bcde8717de0e42
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-03-25 08:17:23 +0530

    Update Subset Sum Problem.java

--- Java-practice/Subset Sum Problem.java
+++ Java-practice/Subset Sum Problem.java
@@ -4,6 +4,36 @@
 
 
 
+
+
+
+//Recursion with Memoization 
+
+
+class Solution {
+    
+    static int calculate(int start,int end,int sum,int dp[][],int arr[]) {
+        if(sum == 0) return 1;
+        if(sum < 0 || start == end) return 0;
+        if(dp[start][sum] != -1) return dp[start][sum];
+        return dp[start][sum] = (calculate(start+1,end,sum,dp,arr) | calculate(start+1,end,sum-arr[start],dp,arr));
+    }
+
+    static Boolean isSubsetSum(int arr[], int sum) {
+        int n = arr.length;
+        int dp[][] = new int[n][sum+1];
+        for(int row[]: dp) {
+            Arrays.fill(row,-1);
+        }
+        int ans = calculate(0,arr.length,sum,dp,arr);
+        return ans == 0? false:true;
+    }
+}
+
+
+
+
+
 //Exponential approach
 
 
