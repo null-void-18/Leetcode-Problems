592b9eeb1f9ef483d188225cb4a713c4fe4ab0d9
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-04-13 21:03:59 +0530

    Update Shortest Common Supersequence.java

--- Java-practice/Shortest Common Supersequence.java
+++ Java-practice/Shortest Common Supersequence.java
@@ -1,4 +1,82 @@
 
+//Better way to print 
+
+
+public class Solution {
+
+
+    public static String findLCS(int l, int q, String text1, String text2){
+        int m = text1.length();
+        int n = text2.length();
+        
+        int dp[][] = new int[m + 1][n + 1]; 
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0; 
+                } else if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1]; 
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); 
+                }
+            }
+        }
+
+        String s = "";
+
+        String reversedStr = "";
+
+        int i = m,j = n;
+
+
+        while(i > 0 && j > 0) {
+            if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                s += text1.charAt(i-1);
+                i--;j--;
+            }
+            else if(dp[i-1][j] == dp[i][j]) {
+                s += text1.charAt(i-1);
+                i--;
+            }
+            else {
+                s += text2.charAt(j-1);
+                j--;
+            }
+        }
+
+        while(i > 0) {
+                s += text1.charAt(i-1);
+                i--;
+        }
+
+        while(j > 0) {
+                s += text2.charAt(j-1);
+                j--;
+        }
+
+        for(int k = s.length() - 1; k >= 0;k--) {
+            reversedStr += s.charAt(k);
+        }
+
+        return reversedStr;
+    }
+
+
+    public static String shortestSupersequence(String a, String b) {
+        int m = a.length();
+        int n = b.length();
+        String lcs = findLCS(a.length(),b.length(),a,b);
+
+        return lcs;
+    }
+
+}
+
+
+
+
+
 //DP Approach with LCS
 
 public class Solution {
