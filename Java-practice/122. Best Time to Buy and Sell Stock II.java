7262be74f07935de4796acf812911e69c6a84247
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-04-20 12:20:51 +0530

    Update 122. Best Time to Buy and Sell Stock II.java

--- Java-practice/122. Best Time to Buy and Sell Stock II.java
+++ Java-practice/122. Best Time to Buy and Sell Stock II.java
@@ -1,3 +1,29 @@
+
+//Best Solution
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i = 0, buy, sell, profit = 0, N = prices.length - 1;
+        while (i < N) {
+            while (i < N && prices[i + 1] <= prices[i]) i++;
+            buy = prices[i];
+
+            while (i < N && prices[i + 1] > prices[i]) i++;
+            sell = prices[i];
+
+            profit += sell - buy;
+        }
+        return profit;
+    }
+}
+
+
+
+
+
+
+//My solution
+
 class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
@@ -19,5 +45,35 @@ class Solution {
         }
 
         return profit;
+    }
+}
+
+
+//Recursion with Memoization 
+
+class Solution {
+    int profit(int dp[][],int prices[],int index,int buy) {
+        if(index == prices.length) {
+            return 0;
+        }
+
+        if(dp[index][buy] != -1) {
+            return dp[index][buy];
+        }
+
+        if(buy == 1) {
+            return dp[index][buy] = Math.max(-prices[index] + profit(dp,prices,index+1,0),profit(dp,prices,index+1,1));
+        }
+        else {
+            return dp[index][buy] = Math.max(prices[index] + profit(dp,prices,index+1,1),profit(dp,prices,index+1,0));
+        }
+    }
+
+    public int maxProfit(int[] prices) {
+        int dp[][] = new int[prices.length][2];
+        for(int row[] : dp) {
+            Arrays.fill(row,-1);
+        }
+        return profit(dp,prices,0,1);
     }
 }
