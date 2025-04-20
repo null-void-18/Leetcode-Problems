86feeae827734c418954d4a9a313740ac3a4dc05
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-04-20 11:49:35 +0530

    Update 121. Best Time to Buy and Sell Stock.java

--- Java-practice/121. Best Time to Buy and Sell Stock.java
+++ Java-practice/121. Best Time to Buy and Sell Stock.java
@@ -9,3 +9,36 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+//Recursion with Memoization
+class Solution { 
+
+    int profit(int dp[][],int prices[],int index,int buy) {
+        if(index == prices.length || buy == 2) {
+            return 0;
+        }
+
+        if(dp[index][buy] != -1) return dp[index][buy];
+
+        if(buy == 1) {
+            return dp[index][buy] = Math.max(-prices[index] + profit(dp,prices,index+1,0),profit(dp,prices,index+1,1));
+        }
+        else {
+            return dp[index][buy] = Math.max(prices[index] + profit(dp,prices,index+1,2),profit(dp,prices,index+1,0));
+        }
+    }
+
+
+    public int maxProfit(int[] prices) {
+        int dp[][] = new int[prices.length][3];
+
+        for(int row[] : dp) {
+            Arrays.fill(row,-1);
+        }
+
+        return profit(dp,prices,0,1);
+    }
+}
