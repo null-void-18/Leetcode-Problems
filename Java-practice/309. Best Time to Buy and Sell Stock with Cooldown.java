0bd88c297ec7b7b94a6990b8fd8573d0956b74a6
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-04-22 11:44:14 +0530

    Update 309. Best Time to Buy and Sell Stock with Cooldown.java

--- Java-practice/309. Best Time to Buy and Sell Stock with Cooldown.java
+++ Java-practice/309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -1,4 +1,44 @@
 
+//DP Approach with Space Optimization
+
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int dp[][][] = new int[prices.length+1][2][2];
+        int after[][] = new int[2][2],curr[][] = new int[2][2];
+
+        for(int row[] : after) {
+            Arrays.fill(row,0);
+        }
+        for(int row[] : curr) {
+            Arrays.fill(row,0);
+        }
+
+        int n = prices.length;
+
+        for(int index = n-1;index >= 0;index--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                for(int cooldown = 1; cooldown >= 0;cooldown--) {
+                    if(buy == 1 && cooldown != 1) {
+                        curr[buy][cooldown] = Math.max(-prices[index] + after[0][0],after[1][0]);
+                    }
+                    else if(cooldown != 1){
+                        curr[buy][cooldown] = Math.max(prices[index] + after[1][1],after[0][0]);
+                    }else {
+                        curr[buy][cooldown] = after[1][0];
+                    }
+                }
+            }
+            for (int i = 0; i < curr.length; i++) {
+                System.arraycopy(curr[i], 0, after[i], 0, curr[i].length);
+            }
+        }
+
+        return after[1][0];
+    }
+}
+
+
 
 
 //DP Approach
