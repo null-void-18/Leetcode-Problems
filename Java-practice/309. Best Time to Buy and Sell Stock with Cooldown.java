a08c872632a7dd74c87d360d0dc883935f2b6ccb
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-04-22 11:40:04 +0530

    Update 309. Best Time to Buy and Sell Stock with Cooldown.java

--- Java-practice/309. Best Time to Buy and Sell Stock with Cooldown.java
+++ Java-practice/309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -1,5 +1,41 @@
 
 
+
+//DP Approach
+
+
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int dp[][][] = new int[prices.length+1][2][2];
+
+        for(int rows[][] : dp) {
+            for(int row[] : rows) {
+                Arrays.fill(row,0);
+            }
+        }
+        int n = prices.length;
+
+        for(int index = n-1;index >= 0;index--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                for(int cooldown = 1; cooldown >= 0;cooldown--) {
+                    if(buy == 1 && cooldown != 1) {
+                        dp[index][buy][cooldown] = Math.max(-prices[index] + dp[index+1][0][0],dp[index+1][1][0]);
+                    }
+                    else if(cooldown != 1){
+                        dp[index][buy][cooldown] = Math.max(prices[index] + dp[index+1][1][1],dp[index+1][0][0]);
+                    }else {
+                        dp[index][buy][cooldown] = dp[index+1][1][0];
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][0];
+    }
+}
+
+
 //Recursion with Memoization
 
 class Solution {
