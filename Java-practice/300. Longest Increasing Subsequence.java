889ab06f9d3757170076fa1dc62cb344d2474e4c
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-04-25 21:38:20 +0530

    Update 300. Longest Increasing Subsequence.java

--- Java-practice/300. Longest Increasing Subsequence.java
+++ Java-practice/300. Longest Increasing Subsequence.java
@@ -21,3 +21,39 @@ class Solution {
         return dp[0][0];
     }
 }
+
+
+
+
+//Recursion with Memoization
+
+class Solution {
+
+
+    public int calculate(int index,int prevIndex,int nums[],int dp[][]) {
+        if(index == nums.length) return 0;
+
+        if(dp[index][prevIndex+1] != -1) return dp[index][prevIndex+1];
+
+        int exclude = calculate(index+1,prevIndex,nums,dp);
+        int include = 0;
+        if(prevIndex == -1 || nums[prevIndex] < nums[index]) {
+            include = 1 + calculate(index+1,index,nums,dp);
+        }
+
+        return dp[index][prevIndex+1] = Math.max(exclude,include);
+    }
+
+
+
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int dp[][] = new int[n][n+1];
+
+        for(int rows[] : dp) {
+            Arrays.fill(rows,-1);
+        }
+
+        return calculate(0,-1,nums,dp);
+    }
+}
