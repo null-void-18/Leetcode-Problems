e373e9867d65c22281d2482f6dd98d0fa9165eee
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-04-26 10:03:09 +0530

    Update 300. Longest Increasing Subsequence.java

--- Java-practice/300. Longest Increasing Subsequence.java
+++ Java-practice/300. Longest Increasing Subsequence.java
@@ -1,3 +1,27 @@
+//Another brilliant solution -- More like insersion sort
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n];
+
+        Arrays.fill(dp,1);
+
+        int ans = 1;
+        for(int i = 1;i < n;i++) {
+            for(int j = 0;j < i;j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(1 + dp[j],dp[i]);
+                }
+            }
+            ans = Math.max(ans,dp[i]);
+        }
+        return ans;
+    }
+}
+
+
+
 
 //Space optimized DP Approach 
 
