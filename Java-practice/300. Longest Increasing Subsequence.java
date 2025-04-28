95d596a0ac17650aca96a8a67873d0d568bfd478
Kiran Sekhar C <30956805+null-void-18@users.noreply.github.com>
2025-04-28 22:51:13 +0530

    Update 300. Longest Increasing Subsequence.java

--- Java-practice/300. Longest Increasing Subsequence.java
+++ Java-practice/300. Longest Increasing Subsequence.java
@@ -1,3 +1,31 @@
+//Most optimal Solution -- Binary Search
+
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> lis = new ArrayList<>();
+
+        for (int num : nums) {
+            int index = Collections.binarySearch(lis, num);
+
+            if (index < 0) {
+                index = -(index + 1);
+            }
+
+            if (index == lis.size()) {
+                lis.add(num);
+            } else {
+                lis.set(index, num);
+            }
+        }
+
+        return lis.size();
+    }
+}
+
+
+
+
+
 //Another brilliant solution -- More like insertion sort
 
 class Solution {
